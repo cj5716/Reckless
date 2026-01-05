c2e19b1146fcfd970be0110475b3d23ac738fa36
cj5716 <125858804+cj5716@users.noreply.github.com>
2026-01-05 09:38:42 +0800

    Bench 4812670

--- src/search.rs
+++ src/search.rs
@@ -496,7 +496,15 @@ fn search<NODE: NodeType>(
 
     // Weirdcut
     let weirdcut_alpha = alpha - 50 - 300 * depth;
-    if !NODE::PV && !in_check && depth > 6 && !cut_node && estimated_score <= weirdcut_alpha && alpha < 2048 {
+    if !NODE::PV
+        && !in_check
+        && depth > 6
+        && !cut_node
+        && !potential_singularity
+        && estimated_score <= weirdcut_alpha
+        && (!is_valid(tt_score) || tt_score <= weirdcut_alpha && !is_decisive(tt_score))
+        && alpha < 2048
+    {
         let score = search::<NonPV>(td, weirdcut_alpha, weirdcut_alpha + 1, depth - 6, false, ply);
         if score <= weirdcut_alpha {
             return score;
