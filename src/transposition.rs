7d1a8afdc5cd57647dc47d992588b361a3486220
cj5716 <125858804+cj5716@users.noreply.github.com>
2026-01-20 07:31:49 +0800

    Revert "Bench 3133258"
    This reverts commit c7b2172ba37b71cbbc6e1049618061687fcbb75e.

--- src/transposition.rs
+++ src/transposition.rs
@@ -59,10 +59,6 @@ impl Flags {
     pub const fn age(self) -> u8 {
         self.data >> 4
     }
-
-    pub fn reset_singular(&mut self) {
-        self.data &= !(1 << 3);
-    }
 }
 
 /// Type of the score returned by the search.
@@ -213,7 +209,6 @@ impl TranspositionTable {
         let entry = replacement_slot.unwrap();
 
         if !(entry.key == key && mv.is_null()) {
-            if entry.mv != mv { entry.flags.reset_singular(); }
             entry.mv = mv;
         }
 
