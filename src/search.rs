45c9b0207aef1f63678e75aee23e101e6fb3f8c3
cj5716 <125858804+cj5716@users.noreply.github.com>
2026-02-21 10:59:50 +0800

    Bench 3527644

--- src/search.rs
+++ src/search.rs
@@ -700,9 +700,9 @@ fn search<NODE: NodeType>(
             // Late Move Pruning (LMP)
             skip_quiets |= !in_check
                 && move_count >= {
-                    let adjust = improvement.clamp(-100, 218);
-                    let factor0 = 2515 + 130 * adjust / 16;
-                    let factor1 = 946 + 79 * adjust / 16;
+                    let adjust = improvement.max(eval - beta - 50).clamp(-100, 218);
+                    let factor0 = 2415 + 130 * adjust / 16;
+                    let factor1 = 906 + 79 * adjust / 16;
 
                     (factor0 + factor1 * depth * depth) / 1024
                 };
