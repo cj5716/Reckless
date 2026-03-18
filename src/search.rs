c814c57db4e0ddf9205dbdc935076d17b94411a1
cj5716 <125858804+cj5716@users.noreply.github.com>
2026-03-18 20:55:57 +0800

    Bench 3132662

--- src/search.rs
+++ src/search.rs
@@ -1078,7 +1078,8 @@ fn search<NODE: NodeType>(
     if !(in_check
         || best_move.is_noisy()
         || (bound == Bound::Upper && best_score >= eval)
-        || (bound == Bound::Lower && best_score <= eval))
+        || (bound == Bound::Lower && best_score <= eval)
+        || (!tt_pv && bound == tt_bound && td.nodes() % 8 == 0))
     {
         update_correction_histories(td, depth, best_score - eval, ply);
     }
