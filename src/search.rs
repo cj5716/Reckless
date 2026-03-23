1cec3128776910657539ac7a6ac04678a43d6b33
cj5716 <125858804+cj5716@users.noreply.github.com>
2026-03-23 16:12:50 +0800

    Bench 3183187

--- src/search.rs
+++ src/search.rs
@@ -728,9 +728,15 @@ fn search<NODE: NodeType>(
                 };
 
             // Futility Pruning (FP)
-            let futility_value = eval + 88 * depth + 63 * history / 1024 + 88 * (eval >= beta) as i32 - 114;
+            let futility_value = eval + 88 * depth + 63 * history / 1024 - 114;
 
-            if !in_check && is_quiet && depth < 14 && futility_value <= alpha && !td.board.is_direct_check(mv) {
+            if !in_check
+                && is_quiet
+                && depth < 14
+                && futility_value <= alpha
+                && eval <= alpha
+                && !td.board.is_direct_check(mv)
+            {
                 if !is_decisive(best_score) && best_score <= futility_value {
                     best_score = futility_value;
                 }
